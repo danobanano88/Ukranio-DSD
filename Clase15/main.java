d3cf2921f3851b31b73d804fb550eca4d2986e81
Daniel Prieto <88695568+danobanano88@users.noreply.github.com>
2022-10-25 21:29:54 -0500

    finalmente quedo

--- Clase15/main.java
+++ Clase15/main.java
@@ -3,14 +3,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,8 +15,7 @@ class Task implements Runnable {
 private String nombre;
 Map<Character, Integer> hm  = new HashMap<Character, Integer>(); 
 
-    public Task(String filename, Map<Character, Integer> map) {
-       hm = map;
+    public Task(String filename) {
        nombre = filename;
     }
 
@@ -74,8 +70,7 @@ Map<Character, Integer> hm  = new HashMap<Character, Integer>();
 
 
 public class main {
-    static Map<Character, Integer> hm  = new HashMap<Character, Integer>(); 
- 
+   
     public static void main(String[] args) {
     
         
@@ -121,18 +116,17 @@ public class main {
         }
 
 
-         // creates five tasks
-         Runnable r1 = new Task("archivo0.txt",hm);
-         Runnable r2 = new Task("archivo1.txt",hm);
-         Runnable r3 = new Task("archivo2.txt",hm);     
-         Runnable r4 = new Task("archivo3.txt",hm);
-         Runnable r5 = new Task("archivo4.txt",hm);
+         
+         Runnable r1 = new Task("archivo0.txt");
+         Runnable r2 = new Task("archivo1.txt");
+         Runnable r3 = new Task("archivo2.txt");     
+         Runnable r4 = new Task("archivo3.txt");
+         Runnable r5 = new Task("archivo4.txt");
            
-         // creates a thread pool with MAX_T no. of 
-         // threads as the fixed pool size(Step 2)
+    
          ExecutorService pool = Executors.newFixedThreadPool(3);  
           
-         // passes the Task objects to the pool to execute (Step 3)
+        
          pool.execute(r1);
          pool.execute(r2);
          pool.execute(r3);
@@ -140,13 +134,8 @@ public class main {
          pool.execute(r5);
 
            
-         // pool shutdown ( Step 4)
+        
          pool.shutdown();
 
     }
 }
-
-     
-        
-
-      
